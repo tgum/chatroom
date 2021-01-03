13512c6059677795b3702f3c3556a670307b9bbb
tegumai <tegumai@protonmail.com>
2021-01-03 15:45:49 +0100

    added notifications, limit of 100 messages.
    added notifications, added a limit of 100 messages and online users.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+if (sessionStorage.user == undefined) {
+	location.href = "index.html"
+}
+
+function sendNotification(notificationMessage) {
+	// Let's check if the browser supports notifications
+	if (!("Notification" in window)) {
+		alert("This browser does not support desktop notification");
+	}
+
+		// Let's check whether notification permissions have already been granted
+	else if (Notification.permission === "granted") {
+		// If it's okay let's create a notification
+			var notification = new Notification(notificationMessage);
+	}
+
+	// Otherwise, we need to ask the user for permission
+	else if (Notification.permission !== "denied") {
+		Notification.requestPermission().then(function (permission) {
+			// If the user accepts, let's create a notification
+			if (permission === "granted") {
+				var notification = new Notification(notificationMessage);
+			}
+		});
+	}
+}
+
 function rot13(str) {
 	var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 	var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
@@ -20,6 +47,7 @@ var database = firebase.database();
 var ref = database.ref("messages");
 var allMessages;
 var time = new Date()
+var firstFetch = true
 ref.on("value", readMessages, errData);
 function encode() {
 	var text = document.getElementById("textBox").value;
@@ -49,17 +77,27 @@ document.getElementById("submit").addEventListener("click", encode);
 
 function readMessages(data) {
 	allMessages = data.val();
-	var keys = Object.keys(allMessages).reverse();;
+	var keys = Object.keys(allMessages).reverse();
+	if (firstFetch == false && allMessages[keys[0]].sender !== sessionStorage.user) {
+		sendNotification("new message by " + allMessages[keys[0]].sender)
+	}
 	document.getElementById("messages").innerHTML = "";
 	for (var i = 0; i < keys.length; i++) {
 		if (allMessages[keys[i]].recipient === sessionStorage.user || allMessages[keys[i]].recipient === undefined || allMessages[keys[i]].sender === sessionStorage.user || allMessages[keys[i]].recipient === null) {
 			var newLi = document.createElement("li");
 			name = "<strong>" + allMessages[keys[i]].sender + "</strong>:<br>"
-			content = "<span class='content' disabled>" + rot13(allMessages[keys[i]].content) + "</span><br>"
+			content = "<span class='content'>" + rot13(allMessages[keys[i]].content) + "</span><br>"
 			time = "<span class='time'>" +  allMessages[keys[i]].time + "</span>"
 			newLi.innerHTML = name + content + time;
 			document.getElementById("messages").appendChild(newLi);
 		}
+	}
+	firstFetch = false
+	console.log(keys.length)
+	if (keys.length > 100) {
+		firstFetch = true
+		let userRef = database.ref('messages/' + keys[keys.length - 1]);
+		userRef.remove()
 	}
 }
 
@@ -77,25 +115,27 @@ function readUsers(data) {
 	onlineUsers = data.val();
 	var keys = Object.keys(onlineUsers)
 	keys = keys.slice(1, keys.length);
-	var onlineUl = document.getElementById("online");
-	onlineUl.innerHTML = ""
+	var onlineP = document.getElementById("online");
+	onlineP.innerHTML = ""
 	for (var i = 0; i < keys.length; i++) {
-		var newUser = document.createElement("li")
-		newUser.innerHTML = "<span class='at'>@</span>" + onlineUsers[keys[i]].name
-		onlineUl.appendChild(newUser)
+		var newUser = "<span class='at'>@</span>" + onlineUsers[keys[i]].name + " "
+		onlineP.innerHTML += newUser
 	}
 }
 
 function onQuit() {
-	onlineUsers = data.val();
-	var keys = Object.keys(onlineUsers)
-	keys = keys.slice(1, keys.length);
-	for (var i = 0; i < keys.length; i++) {
-		if (onlineUsers[keys[i]] == sessionStorage.user) {
-			let userRef = database.ref('online-users/' + keys[i]);
-			userRef.remove()
+	onlineUsersRef = database.ref("online-users")
+	onlineUsersRef.on("value", data => {
+		onlineUsers = data.val();
+		var keys = Object.keys(onlineUsers)
+		keys = keys.slice(1, keys.length);
+		for (var i = 0; i < keys.length; i++) {
+			if (onlineUsers[keys[i]].name == sessionStorage.user) {
+				let userRef = database.ref('online-users/' + keys[i]);
+				userRef.remove()
+			}
 		}
-	}
+	}, errData);
 }
 
 window.onbeforeunload = function(){
