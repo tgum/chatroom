606c9e6640e0d42cfcc0f88a3056f9ec96273aff
tegumai <tegumai@protonmail.com>
2021-01-11 20:37:22 +0100

    fix: fixed enter thing + deleted the direct message bit

--- script.js
+++ script.js
@@ -33,12 +33,6 @@ function sendMessage() {
 		minutes = "0" + minutes.toString();
 	}
 
-	var recipient = null
-	if (text.charAt(0) === "@") {
-		var recipient = text.split(" ")[0].replace("@", "");
-		text = text.replace(text.split(" ")[0], "")
-	}
-
 	if (text === "") {
 		alert("You have to write something")
 		return null;
@@ -48,7 +42,6 @@ function sendMessage() {
 		sender: sessionStorage.user,
 		content: rot13(text),
 		time: time.getHours() + ":" + minutes,
-		recipient: recipient
 	}
 	ref.push(data);
 	document.getElementById("textBox").value = "";
@@ -62,17 +55,15 @@ function readMessages(data) {
 	var keys = Object.keys(allMessages).reverse();
 	document.getElementById("messages").innerHTML = "";
 	for (var i = 0; i < keys.length; i++) {
-		if (allMessages[keys[i]].recipient === sessionStorage.user || allMessages[keys[i]].recipient === undefined || allMessages[keys[i]].sender === sessionStorage.user || allMessages[keys[i]].recipient === null) {
-			var newLi = document.createElement("li");
-			name = "<strong class='user'>" + allMessages[keys[i]].sender + "</strong>:<br>"
-			content = "<div class='content'>" + rot13(allMessages[keys[i]].content) + "</div><br>"
-			time = "<span class='time'>" +  allMessages[keys[i]].time + "</span>"
-			if (content.includes("\n")) {
-				content = content.replaceAll("\n", "<br>")
-			}
-			newLi.innerHTML = name + content.replace("\n", "<br>") + time;
-			document.getElementById("messages").appendChild(newLi);
+		var newLi = document.createElement("li");
+		name = "<strong class='user'>" + allMessages[keys[i]].sender + "</strong>:<br>"
+		content = "<div class='content'>" + rot13(allMessages[keys[i]].content) + "</div><br>"
+		time = "<span class='time'>" +  allMessages[keys[i]].time + "</span>"
+		if (content.includes("\n")) {
+			content = content.replaceAll("\n", "<br>")
 		}
+		newLi.innerHTML = name + content.replace("\n", "<br>") + time;
+		document.getElementById("messages").appendChild(newLi);
 	}
 	if (keys.length > 100) {
 		let userRef = database.ref('messages/' + keys[keys.length - 1]);
@@ -83,57 +74,6 @@ function readMessages(data) {
 function errData(err) {
 	console.error(err);
 }
-
-/*function onQuit() {
-	onlineUsersRef = database.ref("online-users")
-	onlineUsersRef.on("value", data => {
-		onlineUsers = data.val();
-		var keys = Object.keys(onlineUsers)
-		keys = keys.slice(1, keys.length);
-		for (var i = 0; i < keys.length; i++) {
-			if (onlineUsers[keys[i]].name == sessionStorage.user) {
-				let userRef = database.ref('online-users/' + keys[i]);
-				userRef.remove()
-			}
-		}
-	}, errData);
-}
-
-window.onbeforeunload = function(){
-	onQuit();
-};*/
-
-var keys = {
-	shift: false,
-	enter: false
-}
-var input = document.getElementById("textBox")
-var button = document.getElementById("submit");
-document.getElementById("textBox").addEventListener("keydown", function(event) {
-	if (event.keyCode === 16) {
-		event.preventDefault();
-		keys.shift = true
-	}
-	else if (event.keyCode === 13) {
-		event.preventDefault();
-		keys.enter = true;
-	}
-	if (keys.shift && keys.enter) {
-		button.click();
-		keys.enter = false;
-		keys.shift = false;
-	}
-});
-document.getElementById("textBox").addEventListener("keyup", event => {
-	if (event.keyCode === 16) {
-		event.preventDefault();
-		keys.shift = false
-	}
-	else if (event.keyCode === 13) {
-		event.preventDefault();
-		keys.enter = false
-	}
-});
 
 window.onclick = e => {
 	if (e.target.className === "user") {
